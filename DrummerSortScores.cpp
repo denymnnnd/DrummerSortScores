9f2038809aec9cbaf9229a9c3324e38674d2c93b
denymnnnd <150882117+denymnnnd@users.noreply.github.com>
2024-02-09 23:37:16 -0600

    Create DrummerSortScores.cpp

---
+++ DrummerSortScores.cpp
@@ -0,0 +1,24 @@
+//Denymn Drummer
+
+//Dr. R. Knowles
+
+//This program 
+
+
+#include <iostream>
+
+
+using namespace std;
+
+int main (){
+    
+    
+    double *testScores, total; //dynamically allocate arrat
+    int count; //Array index
+    int numberofScores;
+    
+    //Get number of test scores
+    cout << "How many test scores would you like to enter ?";
+    cin >> numberofScores ;
+
+}   testScores = new double[numberofScores];// 
